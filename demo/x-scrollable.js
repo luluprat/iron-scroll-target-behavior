be415cc465f903ed336334e8f437c49fd8d3114e
Elliott Marquez <emarquez@google.com>
2018-03-08 16:08:33 -0800

    auto-converted by polymer-modulizer

--- demo/x-scrollable.js
+++ demo/x-scrollable.js
@@ -1,4 +1,8 @@
-<!--
+import '../../polymer/polymer.js';
+import { IronScrollTargetBehavior } from '../iron-scroll-target-behavior.js';
+import { Polymer } from '../../polymer/lib/legacy/polymer-fn.js';
+import { html } from '../../polymer/lib/utils/html-tag.js';
+/**
 @license
 Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,13 +10,9 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
-
-<link rel="import" href="../../polymer/polymer.html">
-<link rel="import" href="../iron-scroll-target-behavior.html">
-
-<dom-module id="x-scrollable">
-  <template>
+*/
+Polymer({
+  _template: html`
     <style>
       :host {
         display: block;
@@ -45,55 +45,49 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     <template is="dom-repeat" items="[[_getItems(itemCount)]]">
       <div class="item">[[index]]</div>
     </template>
-  </template>
+`,
 
-</dom-module>
+  is: 'x-scrollable',
 
-<script>
-  Polymer({
+  properties: {
 
-    is: 'x-scrollable',
-
-    properties: {
-
-      xScrollTop: {
-        type: Number,
-        readOnly: true,
-        value: 0
-      },
-
-      xScrollLeft: {
-        type: Number,
-        readOnly: true,
-        value: 0
-      },
-
-      itemCount: {
-        type: Number,
-        value: 200
-      }
-
+    xScrollTop: {
+      type: Number,
+      readOnly: true,
+      value: 0
     },
 
-    behaviors: [
-      Polymer.IronScrollTargetBehavior
-    ],
-
-    attached: function() {
-      this._scrollHandler();
+    xScrollLeft: {
+      type: Number,
+      readOnly: true,
+      value: 0
     },
 
-    _scrollHandler: function() {
-      this._setXScrollTop(this._scrollTop);
-      this._setXScrollLeft(this._scrollLeft);
-    },
-
-    _getItems: function(itemCount) {
-      var items = new Array(itemCount);
-      while (itemCount > 0) {
-        items[--itemCount] = true;
-      }
-      return items;
+    itemCount: {
+      type: Number,
+      value: 200
     }
-  });
-</script>
+
+  },
+
+  behaviors: [
+    IronScrollTargetBehavior
+  ],
+
+  attached: function() {
+    this._scrollHandler();
+  },
+
+  _scrollHandler: function() {
+    this._setXScrollTop(this._scrollTop);
+    this._setXScrollLeft(this._scrollLeft);
+  },
+
+  _getItems: function(itemCount) {
+    var items = new Array(itemCount);
+    while (itemCount > 0) {
+      items[--itemCount] = true;
+    }
+    return items;
+  }
+});
